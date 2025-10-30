f9e9ed16330335bd3586e4160a3fccd64c6c0169
Maragi <admin@maragi.is-cool.dev>
2025-10-30 20:03:05 +0000

    Delete scripts directory

--- scripts/events/before/watchdogTerminate.js
+++
@@ -1,9 +0,0 @@
-import * as ms from "@minecraft/server";
-const sleep = (n) => new Promise(acc => ms.system.runTimeout(acc, n));
-function runEventBeforeWatchdogTerminate(res) {
-    ms.system.run(() => {
-        console.warn(`Watchdog attempted to close the world; Reason: ${res.terminateReason}`);
-        res.cancel = true;
-    });
-}
-export default runEventBeforeWatchdogTerminate;
--- scripts/forms.js
+++
@@ -1,4 +0,0 @@
-import * as ms from "@minecraft/server";
-const sleep = (n) => new Promise(acc => ms.system.runTimeout(acc, n));
-import openAdminFormMain from './forms/adminui/openAdminFormMain';
-export default openAdminFormMain;
